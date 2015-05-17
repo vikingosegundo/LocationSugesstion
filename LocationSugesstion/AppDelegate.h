4251a591efe185297b3836987133421605e75068
Manuel Meyer <manuel@vikingosegudo.de>
2015-05-18 01:21:50 +0200

    Initial Commit

---
+++ LocationSugesstion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationSugesstion
+//
+//  Created by Manuel Meyer on 18.05.15.
+//  Copyright (c) 2015 Manuel Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationSugesstion/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LocationSugesstion
+//
+//  Created by Manuel Meyer on 18.05.15.
+//  Copyright (c) 2015 Manuel Meyer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
